0c4737d789f7667f3e791415bf8fce93970cf1f7
XieXiLin158 <xiexilin158@qq.com>
2021-11-14 00:29:47 +0800

    Update:Badges

--- bootstrap.php
+++ bootstrap.php
@@ -22,4 +22,10 @@ return function ($plugin) {
         'icon'  => 'fa-yen-sign',
         'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
     ]);
+    $filter->add('user_badges', function ($badges, $user) {
+        if (MojangVerification::where('permission', $user->permission)->count() == 4) {
+            $badges[] = ['text' => trans('超级管理员'), 'color' => 'yellow'];
+        }
+        return $badges;
+    });
 };
