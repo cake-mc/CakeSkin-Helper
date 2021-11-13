9714ef5ad02286e0c3c32878bbbd235bdafcc349
XieXiLin158 <xiexilin158@qq.com>
2021-11-14 00:36:00 +0800

    Delete

--- bootstrap.php
+++ bootstrap.php
@@ -22,7 +22,4 @@ return function ($plugin) {
         'icon'  => 'fa-yen-sign',
         'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
     ]);
-    if($user = User::where('permission', '2')){
-        Hook::addUserBadge('超级管理员', 'yellow');
-    };
 };
