42490fbe367cb1d6187551a4e841862ad6124d8a
XieXiLin158 <xiexilin158@qq.com>
2021-11-13 21:42:51 +0800

    插件更新:创造帮助文档按钮

--- bootstrap.php
+++ bootstrap.php
@@ -6,3 +6,10 @@ use Illuminate\Contracts\Events\Dispatcher;
 return function (Dispatcher $events, Filter $filter) {
     //
 };
+
+Hook::addMenuItem('user', 0, [
+    'title' => 'CakeSkin\\Lang::menu.docs',
+    'link'  => 'https://help.cakemc.top/',
+    'icon'  => 'fa-book',
+    'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
+]);
