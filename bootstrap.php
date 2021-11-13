5a417c703280e9425f9fa4233e0893d4367fa147
XieXiLin158 <xiexilin158@qq.com>
2021-11-14 00:30:47 +0800

    Fix

--- bootstrap.php
+++ bootstrap.php
@@ -23,7 +23,7 @@ return function ($plugin) {
         'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
     ]);
     $filter->add('user_badges', function ($badges, $user) {
-        if (MojangVerification::where('permission', $user->permission)->count() == 4) {
+        if (where('permission', $user->permission)->count() == 4) {
             $badges[] = ['text' => trans('超级管理员'), 'color' => 'yellow'];
         }
         return $badges;
