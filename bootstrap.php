a3973359f47606ec0d633775c5398c39da8ad345
XieXiLin158 <xiexilin158@qq.com>
2021-11-13 22:54:31 +0800

    Update

--- bootstrap.php
+++ bootstrap.php
@@ -4,13 +4,16 @@ use Blessing\Filter;
 use Illuminate\Contracts\Events\Dispatcher;
 use App\Services\Hook;
 
-return function (Dispatcher $events, Filter $filter) {
-    //
-};
+return function ($plugin) {
+    // $plugin 就是这个插件的实例。
 
-Hook::addMenuItem('user', 0, [
-    'title' => 'CakeSkin\\Helper::menu.docs',
-    'link'  => 'https://help.cakemc.top/',
-    'icon'  => 'fa-book',
-    'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
-]);
+    // 可以像下面这样获取前端资源 URL：
+    // $plugin->assets('something.css');
+
+    Hook::addMenuItem('user', 0, [
+        'title' => 'CakeSkin\\Helper::menu.docs',
+        'link'  => 'https://help.cakemc.top/',
+        'icon'  => 'fa-book',
+        'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
+    ]);
+};
