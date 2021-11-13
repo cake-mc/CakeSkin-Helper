6397466cc6622d628eaaa176ff97757921dbfb06
XieXiLin158 <xiexilin158@qq.com>
2021-11-13 22:14:51 +0800

    Update

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@ return function (Dispatcher $events, Filter $filter) {
 };
 
 Hook::addMenuItem('user', 0, [
-    'title' => 'CakeSkin::menu.docs',
+    'title' => 'CakeSkin\\Helper::menu.docs',
     'link'  => 'https://help.cakemc.top/',
     'icon'  => 'fa-book',
     'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
