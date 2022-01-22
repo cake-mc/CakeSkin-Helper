5b9f5a2ab8453c7c578b20a1179e3e296aa3eaa0
z0z0r4 <z0z0r4@outlook.com>
2022-01-22 02:20:09 -0500

    update languages and add a button

--- bootstrap.php
+++ bootstrap.php
@@ -5,21 +5,22 @@ use Illuminate\Contracts\Events\Dispatcher;
 use App\Services\Hook;
 
 return function ($plugin) {
-    // $plugin 就是这个插件的实例。
-
-    // 可以像下面这样获取前端资源 URL：
-    // $plugin->assets('something.css');
-
-    Hook::addMenuItem('explore', 0, [
+    Hook::addMenuItem('explore', 1, [
         'title' => 'CakeSkin\\Helper::menu.docs',
         'link'  => 'https://help.cakemc.top/',
         'icon'  => 'fa-book',
-        'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
+        'new-tab' => true,
     ]);
     Hook::addMenuItem('user', 7, [
         'title' => 'CakeSkin\\Helper::menu.donate',
         'link'  => 'https://afdian.net/@Big_Cake2577117205',
-        'icon'  => 'fa-yen-sign',
-        'new-tab' => true, // 表示是否在浏览器新标签页中打开链接，默认为 false
+        'icon'  => 'fa-donate',
+        'new-tab' => true,
+    ]);
+    Hook::addMenuItem('explore', 7, [
+        'title' => 'CakeSkin\\Helper::menu.mail',
+        'link'  => 'mailto:bigcake@cakemc.top',
+        'icon'  => 'fa-envelope-open-text',
+        'new-tab' => true,
     ]);
 };
