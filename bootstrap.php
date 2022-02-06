ecdd866963c596c4ca23f96ee4cdd0ade9e8749c
XieXiLin <55585872+XieXiLin158@users.noreply.github.com>
2022-02-06 09:03:35 +0800

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,13 @@ use Illuminate\Contracts\Events\Dispatcher;
 use App\Services\Hook;
 
 return function ($plugin) {
-    Hook::addMenuItem('explore', 1, [
+    Hook::addMenuItem('explore', 5, [
+        'title' => 'CakeSkin\\Helper::menu.user-code',
+        'link'  => 'https://help.cakemc.top/cakeskin/policies/user-code.html',
+        'icon'  => 'fa-envelope-open-text',
+        'new-tab' => true,
+    ]);
+    Hook::addMenuItem('explore', 2, [
         'title' => 'CakeSkin\\Helper::menu.docs',
         'link'  => 'https://help.cakemc.top/',
         'icon'  => 'fa-book',
@@ -17,19 +23,19 @@ return function ($plugin) {
         'icon'  => 'fa-donate',
         'new-tab' => true,
     ]);
-    Hook::addMenuItem('explore', 2, [
+    Hook::addMenuItem('explore', 3, [
         'title' => 'CakeSkin\\Helper::menu.privacy',
         'link'  => 'https://help.cakemc.top/cakeskin/policies/privacy.html',
         'icon'  => 'fa-book',
         'new-tab' => true,
     ]);
-    Hook::addMenuItem('explore', 3, [
+    Hook::addMenuItem('explore', 4, [
         'title' => 'CakeSkin\\Helper::menu.tos',
         'link'  => 'https://help.cakemc.top/cakeskin/policies/tos.html',
         'icon'  => 'fa-book',
         'new-tab' => true,
     ]);
-    Hook::addMenuItem('explore', 4, [
+    Hook::addMenuItem('explore', 5, [
         'title' => 'CakeSkin\\Helper::menu.mail',
         'link'  => 'mailto:bigcake@cakemc.top',
         'icon'  => 'fa-envelope-open-text',
